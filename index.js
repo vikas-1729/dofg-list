4c11ac5d3d858dd98b6439a9cf0f53de35d676e4
vikas.singh <vikas.singh@verse.in>
2022-09-24 22:35:11 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const dogBreedAPI = 'https://dog.ceo/api/breeds/list/all';
+const imageByBreedAPI = 'https://dog.ceo/api/breed/';
 const selectField = $("#breed-list");
+const breedImages = document.getElementById('breed-images');
+
+const MAX_IMAGE = 5;
+
 // var dogBreedProm = fetch(dogBreedAPI);
 
 // dogBreedProm.then((resp)=>{
@@ -9,6 +14,40 @@ const selectField = $("#breed-list");
 // }).catch((err)=>{
 //     console.log('err',err);
 // })
+
+selectField.on('change',async function changeSelectField(e){
+    var data = await fetchImagesFromBreed(selectField.val());
+    var resp = addImages(data);
+});
+
+async function addImages(imgList){
+   if(imgList.length <=0){
+        breedImages.innerHTML = "No Images";
+        return;
+   }
+   imgList = imgList.slice(0,MAX_IMAGE);
+   imgList.map((item)=>{
+    var img = document.createElement('img');
+    $(img).attr('src',item);
+    $(img).attr('height','100px');
+    $(img).attr('width','100px');
+    breedImages.append(img);
+   })
+}
+
+async function fetchImagesFromBreed(breedName)
+{
+    try {
+        var resp = await fetch(`${imageByBreedAPI}${breedName}/images`);
+        var data = await resp.json();
+        return data.message;    
+    } catch (error) {
+        console.log('err',error);   
+    }
+    
+}
+
+
 
 async function fetchAllBreedList(){
     try {
@@ -33,11 +72,10 @@ function addToSelect(data){
         $(option).attr("value",data[i]);
         selectField.append(option);
     }
-
 }
 
 (async function init(){
     var data = await  fetchAllBreedList();
     addToSelect(data);
-
+    selectField.change()
 })();
